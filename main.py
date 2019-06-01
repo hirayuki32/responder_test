e5fa5f17649ddf4c651d9449b54019d4037c00d7
yuki <yuki@yuki-PC.local>
2019-06-01 13:30:12 +0900

    test_webpage_that_containing_requested_data

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import responder
-  
+import time
+
 api = responder.API()
+
 
 @api.route("/hello.html")
 def hello_html(req, resp):
-    my_name = "hirayuki"  # 追記
-    resp.html = api.template('index.html', name=my_name)  # 追記
+    my_name = "hirayuki"
+    resp.html = api.template('index.html', name=my_name)
+
+@api.route("/incoming")
+async def receive_incoming(req, resp):
+    data = await req.media()
+    resp.html = api.template('index.html', name=data)
+    print(data)
 
 api.run(port=3000)
